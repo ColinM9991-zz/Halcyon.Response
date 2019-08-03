f5e63ec6606df642e7a3d6d757da8946eb466dc3
Colin M <colinmbackup@outlook.com>
2019-08-03 17:39:20 +0100

    Add Http Content Extenions

---
+++ ColinM.Halcyon.Response.Http/HttpContentExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using ColinM.Halcyon.Response.JsonConverters;
+using Newtonsoft.Json;
+
+namespace ColinM.Halcyon.Response.Http
+{
+    public static class HttpContentExtensions
+    {
+        public static async Task<HalcyonResponseModel<TModel>> ReadAsHalcyonResponse<TModel>(this HttpContent content)
+            where TModel : class, new()
+        {
+            var rawContents = await content.ReadAsStringAsync();
+
+            var halcyonResponse = JsonConvert.DeserializeObject<HalcyonResponseModel<TModel>>(rawContents, new HalcyonJsonConverter<TModel>());
+
+            return halcyonResponse;
+        }
+    }
+}
